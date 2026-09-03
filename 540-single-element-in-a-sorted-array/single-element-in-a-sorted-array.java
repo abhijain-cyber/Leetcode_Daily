801d4ef87d452249985c8bfa474f167afffb7f31
Aagam Jain <jaagam754@gmail.com>
2026-09-03 23:09:19 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (77.44%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -4,17 +4,17 @@ class Solution {
         int end = nums.length-1;
 
         while(start<end){
-          int mid = (start+end)/2;
+            int mid = (start+end)/2;
 
-          if(mid%2==1){
-            mid--;
-          }  
+            if(mid%2==1){
+                mid--;
+            }
 
-          if(nums[mid] == nums[mid+1]){
-            start = mid+2;
-          }else{
-            end = mid;
-          }
+            if(nums[mid] == nums[mid+1]){
+                start = mid+2;
+            }else{
+                end = mid;
+            }
         }
         return nums[start];
     }
